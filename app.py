6e7269ad812660f559fb1a2832d2b8b09d9becc3
WH1TE-SHAD0W <marek@culak.sk>
2024-03-21 15:45:40 +0000

    imported code from dkit web frontend portfolio about devops

--- app.py
+++ app.py
@@ -1,27 +1,42 @@
-from flask import Flask
+import jinja2
+from flask import *
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_url_path='/static')
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    # template_loader = jinja2.FileSystemLoader(searchpath="./")
+    # template_env = jinja2.Environment(loader=template_loader)
+    # template_file = "myapp/templates/index.html.j2"
+    # template = template_env.get_template(template_file)
+    # page = template.render()
+    return render_template('index.html.j2')
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+# app.run(debug=True, host="0.0.0.0")
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
-
-@app.route("/")
-def hello():
-    return "Hello, World!"
+#
+# @app.route("/action_page")
+# def hello():
+#     return "Hello, World!"
+#
+# @app.route("/devopsEngineerJob")
+# def hello():
+#     return "Hello, World!"
+#
+# @app.route("/how-to")
+# def hello():
+#     return "Hello, World!"
+#
+# @app.route("/introduction")
+# def hello():
+#     return "Hello, World!"
+#
+# @app.route("/simpleQuiz")
+# def hello():
+#     return "Hello, World!"
+#
+# @app.route("/tools")
+# def hello():
+#     return "Hello, World!"
